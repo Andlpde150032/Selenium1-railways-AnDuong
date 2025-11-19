f5ad4f95c08e0fc684915eef23403d9253eeae9a
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-19 22:42:38 +0700

    Refactor Page Objects to use By locators

--- src/main/java/demo/page/RegisterPage.java
+++ src/main/java/demo/page/RegisterPage.java
@@ -8,74 +8,43 @@
  */
 package demo.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class RegisterPage {
 
     private WebDriver driver;
 
-    @FindBy(id = "email")
-    private WebElement emailField;
-
-    @FindBy(name = "password")
-    private WebElement passwordField;
-
-    @FindBy(css = "input#confirmPassword")
-    private WebElement confirmPasswordField;
-
-    @FindBy(xpath = "//input[@id='pid']")
-    private WebElement pidField;
-
-    @FindBy(xpath = "//p[@class='form-actions']/input[@type='submit']")
-    private WebElement registerButton;
+    private By emailField = By.id("email");
+    private By passwordField = By.name("password");
+    private By confirmPasswordField = By.cssSelector("input#confirmPassword");
+    private By pidField = By.xpath("//input[@id='pid']");
+    private By registerButton = By.xpath("//p[@class='form-actions']/input[@type='submit']");
 
     public RegisterPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
     }
 
     public void enterEmail(String email) {
-        emailField.sendKeys(email);
+        driver.findElement(emailField).sendKeys(email);
     }
 
     public void enterPassword(String password) {
-        passwordField.sendKeys(password);
+        driver.findElement(passwordField).sendKeys(password);
     }
 
     public void enterConfirmPassword(String password) {
-        confirmPasswordField.sendKeys(password);
+        driver.findElement(confirmPasswordField).sendKeys(password);
     }
 
     public void enterPid(String pid) {
-        pidField.sendKeys(pid);
+        driver.findElement(pidField).sendKeys(pid);
     }
 
     public void clickRegisterButton() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", registerButton);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        registerButton.click();
-    }
-
-    public void performRegistration(String email, String password, String pid) {
-        System.out.println("Report: Filling registration form...");
-        enterEmail(email);
-        System.out.println("Report: Email field filled.");
-        enterPassword(password);
-        System.out.println("Report: Password field filled.");
-        enterConfirmPassword(password);
-        System.out.println("Report: Confirm Password field filled.");
-        enterPid(pid);
-        System.out.println("Report: PID/Passport Number field filled.");
-        clickRegisterButton();
-        System.out.println("Report: Register button clicked.");
+        js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(registerButton));
+        driver.findElement(registerButton).click();
     }
 }
