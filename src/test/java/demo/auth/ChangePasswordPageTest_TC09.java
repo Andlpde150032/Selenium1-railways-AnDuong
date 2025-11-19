700b281c131cdc087f6b2e845c50f0a1a66d063b
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-19 15:30:49 +0700

    Add TC09-Change-Password test file and page object

---
+++ src/test/java/demo/auth/ChangePasswordPageTest_TC09.java
@@ -0,0 +1,70 @@
+/**
+ * @author      AnDLP
+ * @version     1.0
+ * @since       2025-11-18
+ *
+ * Test module: Change Password
+ * Description: User can change password
+ */
+package demo.auth;
+
+import demo.page.LoginPage;
+import demo.page.ChangePasswordPage;
+import demo.utils.JsonDataReader;
+import demo.utils.PropertiesUtils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Properties;
+
+public class ChangePasswordPageTest_TC09 extends demo.Testbase {
+    private Properties properties;
+    private LoginPage loginPage;
+    private ChangePasswordPage changePasswordPage;
+    private JsonDataReader jsonDataReader;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        properties = PropertiesUtils.loadProperties("src/test/resources/config.properties");
+        jsonDataReader = new JsonDataReader("test-data.json");
+    }
+
+    @Test
+    public void testChangePassword() throws InterruptedException {
+        // Pre-condition: User is logged in with a valid and activated account.
+        // Navigate to the main page
+        driver.get(properties.getProperty("base.url"));
+
+        // Click the Login link on the navbar
+        driver.findElement(By.linkText("Login")).click();
+
+        // Perform login
+        loginPage = new LoginPage(driver);
+        loginPage.performLogin(
+                jsonDataReader.getTestData("login", "email"),
+                jsonDataReader.getTestData("login", "password")
+        );
+        Thread.sleep(3000); // Wait for login to complete
+
+        // Click on "Change Password" tab
+        driver.findElement(By.linkText("Change password")).click();
+        Thread.sleep(3000); // Wait for page to load
+
+        // Perform password change
+        changePasswordPage = new ChangePasswordPage(driver);
+        changePasswordPage.changePassword(
+                jsonDataReader.getTestData("login", "password"), // Old password
+                jsonDataReader.getTestData("new_password", "password"), // New password
+                jsonDataReader.getTestData("new_password", "password")  // Confirm new password
+        );
+
+        Thread.sleep(3000); // Wait for action to complete
+
+        // Verify success message
+        assertTrue(driver.findElement(By.xpath("//h1[contains(text(),'Your password has been updated!')]")).isDisplayed(), "Password change success message is not displayed.");
+        System.out.println("TC09 Passed: User successfully changed password.");
+    }
+}
