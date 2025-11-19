3c093198423f0d3da70b598cdd5d6367534bdfad
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-19 22:42:38 +0700

    Refactor Page Objects to use By locators

--- src/main/java/demo/page/BookTicketPage.java
+++ src/main/java/demo/page/BookTicketPage.java
@@ -8,82 +8,63 @@
  */
 package demo.page;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class BookTicketPage {
 
     private WebDriver driver;
 
-    @FindBy(name = "DepartStation")
-    private WebElement departStationDropdown;
-
-    @FindBy(name = "ArriveStation")
-    private WebElement arriveStationDropdown;
-
-    @FindBy(name = "Date")
-    private WebElement departDateDropdown;
-
-    @FindBy(name = "SeatType")
-    private WebElement seatTypeDropdown;
-
-    @FindBy(name = "TicketAmount")
-    private WebElement ticketAmountDropdown;
-
-    @FindBy(xpath = "//input[@value='Book ticket']")
-    private WebElement bookTicketButton;
+    private By departStationDropdown = By.name("DepartStation");
+    private By arriveStationDropdown = By.name("ArriveStation");
+    private By departDateDropdown = By.name("Date");
+    private By seatTypeDropdown = By.name("SeatType");
+    private By ticketAmountDropdown = By.name("TicketAmount");
+    private By bookTicketButton = By.xpath("//input[@value='Book ticket']");
 
     public BookTicketPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
     }
 
     public String getSelectedDepartStation() {
-        Select select = new Select(departStationDropdown);
+        Select select = new Select(driver.findElement(departStationDropdown));
         return select.getFirstSelectedOption().getText();
     }
 
     public String getSelectedArriveStation() {
-        Select select = new Select(arriveStationDropdown);
+        Select select = new Select(driver.findElement(arriveStationDropdown));
         return select.getFirstSelectedOption().getText();
     }
 
     public void selectDepartStation(String station) {
-        Select select = new Select(departStationDropdown);
+        Select select = new Select(driver.findElement(departStationDropdown));
         select.selectByVisibleText(station);
-        System.out.println("Report: Selected Depart Station: " + station);
     }
 
     public void selectArriveStation(String station) {
-        Select select = new Select(arriveStationDropdown);
+        Select select = new Select(driver.findElement(arriveStationDropdown));
         select.selectByVisibleText(station);
-        System.out.println("Report: Selected Arrive Station: " + station);
     }
 
     public void selectDepartDate(String date) {
-        Select select = new Select(departDateDropdown);
+        Select select = new Select(driver.findElement(departDateDropdown));
         select.selectByVisibleText(date);
-        System.out.println("Report: Selected Depart Date: " + date);
     }
 
     public void selectSeatType(String seatType) {
-        Select select = new Select(seatTypeDropdown);
+        Select select = new Select(driver.findElement(seatTypeDropdown));
         select.selectByVisibleText(seatType);
-        System.out.println("Report: Selected Seat Type: " + seatType);
     }
 
     public void selectTicketAmount(String amount) {
-        Select select = new Select(ticketAmountDropdown);
+        Select select = new Select(driver.findElement(ticketAmountDropdown));
         select.selectByVisibleText(amount);
-        System.out.println("Report: Selected Ticket Amount: " + amount);
     }
 
     public void clickBookTicketButton() {
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", bookTicketButton);
-        bookTicketButton.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(bookTicketButton));
+        driver.findElement(bookTicketButton).click();
     }
 }
