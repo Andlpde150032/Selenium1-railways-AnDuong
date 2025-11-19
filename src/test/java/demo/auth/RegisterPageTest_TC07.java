e2f646dcfdac94f99317ebbb935e98153fc04117
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-19 15:28:54 +0700

    Add TC07-Register-New-Account test file

---
+++ src/test/java/demo/auth/RegisterPageTest_TC07.java
@@ -0,0 +1,57 @@
+/**
+ * @author      AnDLP
+ * @version     1.0
+ * @since       2025-11-18
+ *
+ * Test module: Register
+ * Description: User can create new account
+ */
+package demo.auth;
+
+import demo.page.RegisterPage;
+import demo.utils.JsonDataReader;
+import demo.utils.PropertiesUtils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Properties;
+
+public class RegisterPageTest_TC07 extends demo.Testbase {
+
+    private Properties properties;
+    private RegisterPage registerPage;
+    private JsonDataReader jsonDataReader;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        properties = PropertiesUtils.loadProperties("src/test/resources/config.properties");
+        jsonDataReader = new JsonDataReader("test-data.json");
+    }
+
+    @Test
+    public void testCreateNewAccount() throws InterruptedException {
+        // Navigate to the main page
+        driver.get(properties.getProperty("base.url"));
+
+        // Click the Register link on the navbar
+        driver.findElement(By.linkText("Register")).click();
+
+        // Create an instance of the RegisterPage and perform registration
+        registerPage = new RegisterPage(driver);
+        registerPage.performRegistration(
+                jsonDataReader.getTestData("register", "email"),
+                jsonDataReader.getTestData("register", "password"),
+                jsonDataReader.getTestData("register", "pid")
+        );
+
+        // Add a delay to observe the result
+        Thread.sleep(5000);
+
+        // Verify that the success message is displayed
+        assertTrue(driver.findElement(By.xpath("//h1[contains(text(),'Thank you for registering your account')]")).isDisplayed(), "Registration success message is not displayed.");
+        System.out.println("TC07 Passed: User successfully created a new account.");
+    }
+}
