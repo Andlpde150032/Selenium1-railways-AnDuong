c27fae901b4db57461d2917dc322a765ff774a57
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-19 22:17:41 +0700

    Refactor: Move hardcoded UI element texts and partial URLs to test-data.json for TC06-Login-Additional-Pages

--- src/test/java/demo/auth/LoginPageTest_TC06.java
+++ src/test/java/demo/auth/LoginPageTest_TC06.java
@@ -36,7 +36,7 @@ public class LoginPageTest_TC06 extends demo.Testbase {
         driver.get(properties.getProperty("base.url"));
 
         // Click the Login link on the navbar
-        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "loginLinkText"))).click();
 
         // Create an instance of the LoginPage page object
         loginPage = new LoginPage(driver);
@@ -48,20 +48,20 @@ public class LoginPageTest_TC06 extends demo.Testbase {
         Thread.sleep(5000);
 
         // Verify the presence of "My ticket", "Change password" and "Logout" tabs
-        assertTrue(driver.findElement(By.linkText("My ticket")).isDisplayed(), "My ticket tab is not displayed.");
-        assertTrue(driver.findElement(By.linkText("Change password")).isDisplayed(), "Change password tab is not displayed.");
-        assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed(), "Logout tab is not displayed.");
+        assertTrue(driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "myTicketLinkText"))).isDisplayed(), "My ticket tab is not displayed.");
+        assertTrue(driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "changePasswordLinkText"))).isDisplayed(), "Change password tab is not displayed.");
+        assertTrue(driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "logoutLinkText"))).isDisplayed(), "Logout tab is not displayed.");
 
         // Verify redirection for "My ticket"
-        driver.findElement(By.linkText("My ticket")).click();
+        driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "myTicketLinkText"))).click();
         Thread.sleep(3000);
-        assertTrue(driver.getCurrentUrl().contains("/Ticket/Manage.cshtml"), "Not redirected to My ticket page.");
+        assertTrue(driver.getCurrentUrl().contains(jsonDataReader.getTestData("urls", "myTicketPageUrlPartial")), "Not redirected to My ticket page.");
         driver.navigate().back(); // Go back to home page
 
         // Verify redirection for "Change password"
-        driver.findElement(By.linkText("Change password")).click();
+        driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "changePasswordLinkText"))).click();
         Thread.sleep(3000);
-        assertTrue(driver.getCurrentUrl().contains("/Account/ChangePassword.cshtml"), "Not redirected to Change password page.");
+        assertTrue(driver.getCurrentUrl().contains(jsonDataReader.getTestData("urls", "changePasswordPageUrlPartial")), "Not redirected to Change password page.");
 
         System.out.println("TC06 Passed: Additional pages displayed and redirects correctly after login.");
     }
