2a13c1bbb8fd9c4ff9ff0a3d87d1c5ebd34bd813
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-20 13:45:41 +0700

    remove unused imports

--- src/test/java/demo/auth/ChangePasswordPageTest_TC09.java
+++ src/test/java/demo/auth/ChangePasswordPageTest_TC09.java
@@ -51,36 +51,36 @@ public class ChangePasswordPageTest_TC09 extends demo.Testbase {
         driver.findElement(By.linkText(jsonDataReader.getTestData("uiElements", "loginLinkText"))).click();
 
         // Perform login
-                loginPage = new LoginPage(driver);
-                loginPage.enterEmail(
-                        jsonDataReader.getTestData("login", "email")
-                );
-                loginPage.enterPassword(
-                        jsonDataReader.getTestData("login", "password")
-                );
-                loginPage.clickLoginButton();
+        loginPage = new LoginPage(driver);
+        loginPage.enterEmail(
+                jsonDataReader.getTestData("login", "email")
+        );
+        loginPage.enterPassword(
+                jsonDataReader.getTestData("login", "password")
+        );
+        loginPage.clickLoginButton();
 
-                // Verify redirection to homepage
-                // String expectedUrl = properties.getProperty("base.url") + HomePage.HOMEPAGE_PATH;
-                // assertEquals(expectedUrl, driver.getCurrentUrl(), "User was not redirected to the homepage after login.");
+        // Verify redirection to homepage
+        // String expectedUrl = properties.getProperty("base.url") + HomePage.HOMEPAGE_PATH;
+        // assertEquals(expectedUrl, driver.getCurrentUrl(), "User was not redirected to the homepage after login.");
 
 
-                homePage = new HomePage(driver);
-                                assertNotNull(homePage.getTitle(), "HomePage title should not be null after login.");
-                
-                                // Navigate directly to the Change Password page
-                                driver.get(properties.getProperty("base.url") + "/Account/ChangePassword.cshtml");
-                        
-                                // Perform password change
-                                changePasswordPage = new ChangePasswordPage(driver);
-                changePasswordPage.changePassword(
-                        jsonDataReader.getTestData("login", "password"), // Old password
-                        jsonDataReader.getTestData("new_password", "password"), // New password
-                        jsonDataReader.getTestData("new_password", "password")  // Confirm new password
-                );
-        
-                // Verify success message
-                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'" + jsonDataReader.getTestData("changePasswordMessages", "passwordChangeSuccessMessage") + "')]")));
-                assertTrue(driver.findElement(By.xpath("//*[contains(text(),'" + jsonDataReader.getTestData("changePasswordMessages", "passwordChangeSuccessMessage") + "')]")).isDisplayed(), "Password change success message is not displayed.");
+        homePage = new HomePage(driver);
+        assertNotNull(homePage.getTitle(), "HomePage title should not be null after login.");
+
+        // Navigate directly to the Change Password page
+        driver.get(properties.getProperty("base.url") + "/Account/ChangePassword.cshtml");
+
+        // Perform password change
+        changePasswordPage = new ChangePasswordPage(driver);
+        changePasswordPage.changePassword(
+                jsonDataReader.getTestData("login", "password"), // Old password
+                jsonDataReader.getTestData("new_password", "password"), // New password
+                jsonDataReader.getTestData("new_password", "password")  // Confirm new password
+        );
+
+        // Verify success message
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'" + jsonDataReader.getTestData("changePasswordMessages", "passwordChangeSuccessMessage") + "')]")));
+        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'" + jsonDataReader.getTestData("changePasswordMessages", "passwordChangeSuccessMessage") + "')]")).isDisplayed(), "Password change success message is not displayed.");
     }
 }
