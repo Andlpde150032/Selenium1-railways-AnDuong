5df5a75b391710458cd27f042cd84d6dac533d6d
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-23 18:19:38 +0700

    Implement TC03: Login with invalid password

---
+++ src/test/java/com/company/project/tests/auth/LoginPageTest_TC03.java
@@ -0,0 +1,28 @@
+package com.company.project.tests.auth;
+
+import com.company.project.base.BaseTest;
+import com.company.project.pages.HomePage;
+import com.company.project.pages.LoginPage;
+import com.company.project.utils.JsonReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest_TC03 extends BaseTest {
+
+    @Test(description = "User cannot log into Railway with invalid password")
+    public void TC03() {
+        HomePage homePage = new HomePage();
+        homePage.open();
+
+        LoginPage loginPage = homePage.goToLoginPage();
+
+        String email = JsonReader.getData("login", "email");
+        String invalidPassword = "InvalidPassword123";
+
+        loginPage.login(email, invalidPassword);
+
+        String errorMessage = loginPage.getErrorMessage();
+        Assert.assertEquals(errorMessage, "There was a problem with your login and/or errors exist in your form.",
+                "Error message is not displayed correctly");
+    }
+}
