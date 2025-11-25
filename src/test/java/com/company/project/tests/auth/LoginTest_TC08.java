72267998bbe1dfad43fc0038a04165e2dcef14e7
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-25 22:55:56 +0700

    Implement TC08: User cannot login with unactivated account

---
+++ src/test/java/com/company/project/tests/auth/LoginTest_TC08.java
@@ -0,0 +1,36 @@
+package com.company.project.tests.auth;
+
+import com.company.project.base.BaseTest;
+import com.company.project.pages.HomePage;
+import com.company.project.pages.LoginPage;
+import com.company.project.pages.RegisterPage;
+import com.company.project.utils.JsonReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class LoginTest_TC08 extends BaseTest {
+
+    @Test(description = "TC08 - User can't login with an account hasn't been activated")
+    public void TC08_UserCannotLoginWithUnactivatedAccount() {
+        HomePage homePage = new HomePage();
+        homePage.open();
+
+        RegisterPage registerPage = homePage.goToRegisterPage();
+
+        String randomEmail = "new" + new Random().nextInt(100000) + "@example.com";
+        String password = JsonReader.getData("register", "password");
+        String pid = JsonReader.getData("register", "pid");
+
+        registerPage.register(randomEmail, password, password, pid);
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        loginPage.login(randomEmail, password);
+
+        String actualMessage = loginPage.getErrorMessage();
+        String expectedMessage = "Invalid username or password. Please try again.";
+
+        Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match!");
+    }
+}
