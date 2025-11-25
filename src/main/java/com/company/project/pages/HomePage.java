c82b008097fa12a79aced676f42d1cb05ae9eec0
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-25 10:12:45 +0700

    Refactor: Improve HomePage locators to use href attributes

--- src/main/java/com/company/project/pages/HomePage.java
+++ src/main/java/com/company/project/pages/HomePage.java
@@ -1,11 +1,15 @@
 package com.company.project.pages;
-
 import com.company.project.config.PropertiesLoader;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
-    private final By loginTab = By.xpath("//span[contains(text(),'Login')]");
+    private final By loginTab = By.xpath("//a[contains(@href, 'Login.cshtml')]");
+    private final By registerTab = By.xpath("//a[contains(@href, 'Register.cshtml')]");
+    private final By bookTicketTab = By.xpath("//a[contains(@href, 'BookTicketPage.cshtml')]");
+    private final By myTicketTab = By.xpath("//a[contains(@href, 'ManageTicket.cshtml')]");
+    private final By changePasswordTab = By.xpath("//a[contains(@href, 'ChangePassword.cshtml')]");
+    private final By logoutTab = By.xpath("//a[contains(@href, 'Logout')]");
 
     public void open() {
         getDriver().get(PropertiesLoader.getProperty("base.url"));
