315060ae6ef4d23428fb7f6fa3290ea572a8dab9
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-26 21:08:07 +0700

    Standardize TC09: Update infrastructure and email generation

--- src/test/java/com/company/project/tests/auth/ChangePasswordTest_TC09.java
+++ src/test/java/com/company/project/tests/auth/ChangePasswordTest_TC09.java
@@ -9,7 +9,7 @@ import com.company.project.utils.JsonReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Random;
+import com.company.project.utils.TestUtils;
 
 public class ChangePasswordTest_TC09 extends BaseTest {
 
@@ -20,7 +20,7 @@ public class ChangePasswordTest_TC09 extends BaseTest {
 
         RegisterPage registerPage = homePage.goToRegisterPage();
 
-        String randomEmail = "tc09_user_" + new Random().nextInt(100000) + "@example.com";
+        String randomEmail = TestUtils.generateRandomEmail();
         String password = JsonReader.getData("register", "password");
         String pid = JsonReader.getData("register", "pid");
 
