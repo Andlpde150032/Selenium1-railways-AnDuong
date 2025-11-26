c02ce41b7aaa4ffbf3ef6f0b4148f656d73af566
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-26 10:52:02 +0700

    Restore all TC test files from individual branches

---
+++ src/test/java/com/company/project/tests/auth/ChangePasswordTest_TC09.java
@@ -0,0 +1,42 @@
+package com.company.project.tests.auth;
+
+import com.company.project.base.BaseTest;
+import com.company.project.pages.ChangePasswordPage;
+import com.company.project.pages.HomePage;
+import com.company.project.pages.LoginPage;
+import com.company.project.pages.RegisterPage;
+import com.company.project.utils.JsonReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class ChangePasswordTest_TC09 extends BaseTest {
+
+    @Test(description = "TC09 - User can change password")
+    public void TC09_UserCanChangePassword() {
+        HomePage homePage = new HomePage();
+        homePage.open();
+
+        RegisterPage registerPage = homePage.goToRegisterPage();
+
+        String randomEmail = "tc09_user_" + new Random().nextInt(100000) + "@example.com";
+        String password = JsonReader.getData("register", "password");
+        String pid = JsonReader.getData("register", "pid");
+
+        registerPage.register(randomEmail, password, password, pid);
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        loginPage.login(randomEmail, password);
+
+        ChangePasswordPage changePasswordPage = homePage.goToChangePasswordPage();
+
+        String newPassword = "NewPassword123";
+        changePasswordPage.changePassword(password, newPassword, newPassword);
+
+        String actualMessage = changePasswordPage.getSuccessMessage();
+        String expectedMessage = "Your password has been updated!";
+
+        Assert.assertEquals(actualMessage, expectedMessage, "Success message does not match!");
+    }
+}
---
+++ src/test/java/com/company/project/tests/auth/LoginBlankUsernameTest.java
@@ -0,0 +1,34 @@
+/**
+ * @author AnDuong
+ * @date 2025-11-25
+ * @project Selenium1-railways-AnDuong
+ * @function LoginBlankUsernameTest - Test class for Login with blank username
+ */
+package com.company.project.tests.auth;
+
+import com.company.project.base.BaseTest;
+import com.company.project.models.User;
+import com.company.project.pages.HomePage;
+import com.company.project.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginBlankUsernameTest extends BaseTest {
+
+    @Test(description = "User can't login with blank 'Username' textbox")
+    public void testLoginWithBlankUsername() {
+        HomePage homePage = new HomePage();
+        homePage.open();
+
+        LoginPage loginPage = homePage.goToLoginPage();
+
+        User user = User.getValidUser();
+
+        // Enter valid password but leave username blank
+        loginPage.login("", user.getPassword());
+
+        String errorMessage = loginPage.getErrorMessage();
+        Assert.assertEquals(errorMessage, "There was a problem with your login and/or errors exist in your form.",
+                "Error message is not displayed correctly");
+    }
+}
