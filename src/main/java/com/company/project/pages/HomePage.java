499fea125402a18063069ebf0a6687844a13f77e
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-26 22:36:54 +0700

    Fix TC02: Restore HomePage and remove unused pages

--- src/main/java/com/company/project/pages/HomePage.java
+++ src/main/java/com/company/project/pages/HomePage.java
@@ -1,4 +1,3 @@
-
 package com.company.project.pages;
 
 import com.company.project.config.PropertiesLoader;
@@ -7,8 +6,19 @@ import org.openqa.selenium.By;
 /**
  * @author AnDuong
  * @date 2025-11-25
+ * @project Selenium1-railways-AnDuong
+ * @function HomePage - Page object for Home Page
+ */
+public class HomePage extends BasePage {
+
+    private final By loginTab = By.xpath("//a[contains(@href, 'Login.cshtml')]");
+
+    public void open() {
+        getDriver().get(PropertiesLoader.getProperty("base.url"));
+    }
+
+    public LoginPage goToLoginPage() {
         click(loginTab);
         return new LoginPage();
     }
-
-    
+}
