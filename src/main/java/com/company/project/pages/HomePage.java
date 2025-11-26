e69f09e328f37068aacfa2eef82fed91733353d6
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-26 22:38:49 +0700

    Cleanup TC05: Remove unused POM pages and HomePage methods

--- src/main/java/com/company/project/pages/HomePage.java
+++ src/main/java/com/company/project/pages/HomePage.java
@@ -1,4 +1,3 @@
-
 package com.company.project.pages;
 
 import com.company.project.config.PropertiesLoader;
@@ -13,12 +12,6 @@ import org.openqa.selenium.By;
 public class HomePage extends BasePage {
 
     private final By loginTab = By.xpath("//a[contains(@href, 'Login.cshtml')]");
-    private final By registerTab = By.xpath("//a[contains(@href, 'Register.cshtml')]");
-    private final By bookTicketTab = By.xpath("//a[contains(@href, 'BookTicketPage.cshtml')]");
-    private final By myTicketTab = By.xpath("//a[contains(@href, 'ManageTicket.cshtml')]");
-    private final By changePasswordTab = By.xpath("//a[contains(@href, 'ChangePassword.cshtml')]");
-    private final By logoutTab = By.xpath("//a[contains(@href, 'Logout')]");
-    private final By timetableTab = By.xpath("//a[contains(@href, 'TrainTimeListPage.cshtml')]");
 
     public void open() {
         getDriver().get(PropertiesLoader.getProperty("base.url"));
@@ -29,44 +22,4 @@ public class HomePage extends BasePage {
         return new LoginPage();
     }
 
-    public void clickBookTicketTab() {
-        click(bookTicketTab);
-    }
-
-    public RegisterPage goToRegisterPage() {
-        click(registerTab);
-        return new RegisterPage();
-    }
-
-    public ChangePasswordPage goToChangePasswordPage() {
-        click(changePasswordTab);
-        return new ChangePasswordPage();
-    }
-
-    public BookTicketPage goToBookTicketPage() {
-        click(bookTicketTab);
-        return new BookTicketPage();
-    }
-
-    public TimetablePage goToTimetablePage() {
-        click(timetableTab);
-        return new TimetablePage();
-    }
-
-    public MyTicketPage goToMyTicketPage() {
-        click(myTicketTab);
-        return new MyTicketPage();
-    }
-
-    public boolean isMyTicketTabDisplayed() {
-        return !getText(myTicketTab).isEmpty();
-    }
-
-    public boolean isChangePasswordTabDisplayed() {
-        return !getText(changePasswordTab).isEmpty();
-    }
-
-    public boolean isLogoutTabDisplayed() {
-        return !getText(logoutTab).isEmpty();
-    }
 }
