efe43071908ffd558607426e8b45606c67304514
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-27 14:01:36 +0700

    update MyTicketTest.java

--- src/test/java/com/company/project/tests/myticket/MyTicketTest.java
+++ src/test/java/com/company/project/tests/myticket/MyTicketTest.java
@@ -1,11 +1,12 @@
 package com.company.project.tests.myticket;
 
 import com.company.project.base.BaseTest;
-import com.company.project.models.User;
 import com.company.project.pages.BookTicketPage;
 import com.company.project.pages.HomePage;
 import com.company.project.pages.LoginPage;
 import com.company.project.pages.MyTicketPage;
+import com.company.project.pages.RegisterPage;
+import com.company.project.utils.JsonReader;
 import com.company.project.utils.TestUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -23,32 +24,37 @@ public class MyTicketTest extends BaseTest {
         HomePage homePage = new HomePage();
         homePage.open();
 
-        LoginPage loginPage = homePage.goToLoginPage();
+        RegisterPage registerPage = homePage.goToRegisterPage();
 
-        User user = User.getValidUser();
-        loginPage.login(user.getEmail(), user.getPassword());
+        String email = TestUtils.generateRandomEmail();
+        String password = JsonReader.getData("register", "password");
+        String pid = JsonReader.getData("register", "pid");
+
+        registerPage.register(email, password, password, pid);
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        loginPage.login(email, password);
 
         BookTicketPage bookTicketPage = homePage.goToBookTicketPage();
 
-        String departStation = "Sài Gòn";
-        String arriveStation = "Nha Trang";
-        String seatType = "Soft seat with air conditioner";
-        String ticketAmount = "1";
+        String departStation = JsonReader.getData("tc16", "departStation");
+        String arriveStation = JsonReader.getData("tc16", "arriveStation");
+        String seatType = JsonReader.getData("tc16", "seatType");
+        String ticketAmount = JsonReader.getData("tc16", "ticketAmount");
 
         bookTicketPage.bookTicket(departStation, arriveStation, seatType, ticketAmount);
 
         MyTicketPage myTicketPage = homePage.goToMyTicketPage();
-        
 
-        Assert.assertTrue(myTicketPage.isTicketDisplayed(departStation, arriveStation), "Ticket not found before cancellation");
+        // Verify ticket exists before cancelling
+        Assert.assertTrue(myTicketPage.isTicketDisplayed(departStation, arriveStation),
+                "Ticket not found before cancellation");
 
-                
-                
         myTicketPage.cancelTicket(departStation, arriveStation);
 
         // Verify ticket is removed
-        Assert.assertFalse(myTicketPage.isTicketDisplayed(departStation, arriveStation), "Ticket still displayed after cancellation");
+        Assert.assertFalse(myTicketPage.isTicketDisplayed(departStation, arriveStation),
+                "Ticket still displayed after cancellation");
     }
-                
-                
+
 }
