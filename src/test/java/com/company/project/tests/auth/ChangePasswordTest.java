ab931b7fb2da9d534ff12d939732e5f234ff1601
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-27 10:44:33 +0700

    Prune ChangePasswordTest to only contain TC09

--- src/test/java/com/company/project/tests/auth/ChangePasswordTest.java
+++ src/test/java/com/company/project/tests/auth/ChangePasswordTest.java
@@ -1,12 +1,11 @@
 package com.company.project.tests.auth;
 
 import com.company.project.base.BaseTest;
+import com.company.project.models.User;
 import com.company.project.pages.ChangePasswordPage;
 import com.company.project.pages.HomePage;
 import com.company.project.pages.LoginPage;
-import com.company.project.pages.RegisterPage;
 import com.company.project.utils.JsonReader;
-import com.company.project.utils.TestUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,8 +13,7 @@ import org.testng.annotations.Test;
  * @author AnDuong
  * @date 2025-11-25
  * @project Selenium1-railways-AnDuong
- * @function ChangePasswordTest - Test class for Change Password functionality
- *           (TC09)
+ * @function ChangePasswordTest - Test class for Change Password functionality (TC09)
  */
 public class ChangePasswordTest extends BaseTest {
 
@@ -24,25 +22,18 @@ public class ChangePasswordTest extends BaseTest {
         HomePage homePage = new HomePage();
         homePage.open();
 
-        RegisterPage registerPage = homePage.goToRegisterPage();
-
-        String randomEmail = TestUtils.generateRandomEmail();
-        String password = JsonReader.getData("register", "password");
-        String pid = JsonReader.getData("register", "pid");
-
-        registerPage.register(randomEmail, password, password, pid);
-
         LoginPage loginPage = homePage.goToLoginPage();
-        loginPage.login(randomEmail, password);
+
+        User user = User.getValidUser();
+        loginPage.login(user.getEmail(), user.getPassword());
 
         ChangePasswordPage changePasswordPage = homePage.goToChangePasswordPage();
 
-        String newPassword = "NewPassword123";
-        changePasswordPage.changePassword(password, newPassword, newPassword);
+        String newPassword = JsonReader.getData("register", "password");
+        changePasswordPage.changePassword(user.getPassword(), newPassword, newPassword);
 
-        String actualMessage = changePasswordPage.getSuccessMessage();
-        String expectedMessage = "Your password has been updated!";
-
-        Assert.assertEquals(actualMessage, expectedMessage, "Success message does not match!");
+        String successMessage = changePasswordPage.getSuccessMessage();
+        Assert.assertEquals(successMessage, "Your password has been updated!",
+                "Success message is not displayed correctly");
     }
 }
