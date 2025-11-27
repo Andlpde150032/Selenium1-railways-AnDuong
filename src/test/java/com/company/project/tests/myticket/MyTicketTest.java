cfcc1bd82928e60f4356cce69b6b136314952460
Andlpde150032 <phuanbeu530@gmail.com>
2025-11-27 11:00:39 +0700

    Prune MyTicketTest to only contain TC16

--- src/test/java/com/company/project/tests/myticket/MyTicketTest.java
+++ src/test/java/com/company/project/tests/myticket/MyTicketTest.java
@@ -1,11 +1,11 @@
 package com.company.project.tests.myticket;
 
+import com.company.project.base.BaseTest;
+import com.company.project.models.User;
 import com.company.project.pages.BookTicketPage;
 import com.company.project.pages.HomePage;
 import com.company.project.pages.LoginPage;
 import com.company.project.pages.MyTicketPage;
-import com.company.project.base.BaseTest;
-import com.company.project.utils.JsonReader;
 import com.company.project.utils.TestUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -20,40 +20,32 @@ public class MyTicketTest extends BaseTest {
 
     @Test(description = "TC16 - User can cancel a ticket")
     public void TC16_UserCanCancelTicket() {
-        String email = TestUtils.generateRandomEmail();
-        String password = JsonReader.getData("register", "password");
-        String pid = JsonReader.getData("register", "pid");
-        String departStation = JsonReader.getData("tc16", "departStation");
-        String arriveStation = JsonReader.getData("tc16", "arriveStation");
-        String seatType = JsonReader.getData("tc16", "seatType");
-        String ticketAmount = JsonReader.getData("tc16", "ticketAmount");
-
         HomePage homePage = new HomePage();
         homePage.open();
 
-        // Register and Login
-        homePage.goToRegisterPage()
-                .register(email, password, password, pid);
-
         LoginPage loginPage = homePage.goToLoginPage();
-        loginPage.login(email, password);
 
-        // Book a ticket
+        User user = User.getValidUser();
+        loginPage.login(user.getEmail(), user.getPassword());
+
         BookTicketPage bookTicketPage = homePage.goToBookTicketPage();
-        bookTicketPage.bookTicket(departStation, arriveStation, seatType, ticketAmount);
 
-        // Go to My Ticket page
+        String departDate = TestUtils.getFutureDate(5);
+        String departStation = "Sài Gòn";
+        String arriveStation = "Nha Trang";
+        String seatType = "Soft seat with air conditioner";
+        String ticketAmount = "1";
+
+        bookTicketPage.bookTicket(departDate, departStation, arriveStation, seatType, ticketAmount);
+
         MyTicketPage myTicketPage = homePage.goToMyTicketPage();
+        
+        // Verify ticket exists before cancelling
+        Assert.assertTrue(myTicketPage.isTicketDisplayed(departStation, arriveStation, departDate, seatType, ticketAmount), "Ticket not found before cancellation");
 
-        // Verify ticket is displayed before cancelling
-        Assert.assertTrue(myTicketPage.isTicketDisplayed(departStation, arriveStation),
-                "Ticket should be displayed in My Ticket page before cancellation");
-
-        // Cancel the ticket
-        myTicketPage.cancelTicket(departStation, arriveStation);
+        myTicketPage.cancelTicket(departStation, arriveStation, departDate, seatType, ticketAmount);
 
         // Verify ticket is removed
-        Assert.assertFalse(myTicketPage.isTicketDisplayed(departStation, arriveStation),
-                "Ticket should be removed from My Ticket page after cancellation");
+        Assert.assertFalse(myTicketPage.isTicketDisplayed(departStation, arriveStation, departDate, seatType, ticketAmount), "Ticket still displayed after cancellation");
     }
 }
